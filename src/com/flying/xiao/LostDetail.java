4666ad9acd8bf366d4f64f0c952cddf25ab365a7
xzj <214746944@qq.com>
2014-08-28 10:32:00 +0800

    图片浏览全完成

--- src/com/flying/xiao/LostDetail.java
+++ src/com/flying/xiao/LostDetail.java
@@ -30,10 +30,9 @@ import com.flying.xiao.util.ImageManager2;
  * lost详情
  * 
  * @author zhangmin
- *
+ * 
  */
-public class LostDetail extends BaseActivity
-{
+public class LostDetail extends BaseActivity {
 
 	private final static int DATA_LOAD_ING = 0x001;
 	private final static int DATA_LOAD_COMPLETE = 0x002;
@@ -59,10 +58,10 @@ public class LostDetail extends BaseActivity
 	private List<XImage> imageUrls;
 	private List<View> images;
 	private Resources resources;
+	private ArrayList<String> imageList;// URLS
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState)
-	{
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.lost_detail);
 		resources = this.getResources();
@@ -73,31 +72,25 @@ public class LostDetail extends BaseActivity
 	}
 
 	@Override
-	protected void initHeadView()
-	{
+	protected void initHeadView() {
 		super.initHeadView();
 		mHeadProgressBar.setVisibility(View.VISIBLE);
 		mHeadRightView.setVisibility(View.GONE);
 		mHeadTitle.setText("详情");
 	}
 
-	private void initView()
-	{
-		if (getIntent().getSerializableExtra("contentObject") != null)
-		{
+	private void initView() {
+		if (getIntent().getSerializableExtra("contentObject") != null) {
 			con = (XContent) getIntent().getSerializableExtra("contentObject");
-		} else
-		{
+		} else {
 			int index = getIntent().getIntExtra("content", 0);
 			con = appContext.listManager.getLostContentList().get(index);
 		}
 
 		imageUrls = con.getImages();
 		images = new ArrayList<View>();
-		if (imageUrls != null && imageUrls.size() > 0)
-		{
-			for (XImage image : imageUrls)
-			{
+		if (imageUrls != null && imageUrls.size() > 0) {
+			for (XImage image : imageUrls) {
 				String url = URLs.HOST + image.getImageUrl();
 				ImageView imageView = new ImageView(this);
 				// imageView.setScaleType(ScaleType.FIT_XY);
@@ -106,13 +99,23 @@ public class LostDetail extends BaseActivity
 				images.add(imageView);
 			}
 		}
+
+		int size = 0;
+		if (imageUrls != null && (size = imageUrls.size()) > 0) {
+			imageList = new ArrayList<String>();
+			for (XImage image : imageUrls) {
+				imageList.add(URLs.HOST + image.getImageUrl());
+			}
+		}
 		indexView = (TextView) findViewById(R.id.showpagerindex);
 		indexView.setText("1/" + images.size());
 
 		if (con.isMeCollecte())
-			mHeadRightView.setImageDrawable(resources.getDrawable(R.drawable.head_favorite_y));
+			mHeadRightView.setImageDrawable(resources
+					.getDrawable(R.drawable.head_favorite_y));
 		else
-			mHeadRightView.setImageDrawable(resources.getDrawable(R.drawable.head_favorite));
+			mHeadRightView.setImageDrawable(resources
+					.getDrawable(R.drawable.head_favorite));
 		mHeadRightView.setOnClickListener(collectClickListener);
 
 		mPlace = (TextView) findViewById(R.id.lost_detail_place);
@@ -132,28 +135,26 @@ public class LostDetail extends BaseActivity
 		mSendMsg.setOnClickListener(listener);
 		mWriteWords.setOnClickListener(listener);
 		mTitle.setText(con.getConTitle());
-		mPubTime.setText(StringUtils.friendly_time(con.getConPubTime().toString()));
+		mPubTime.setText(StringUtils.friendly_time(con.getConPubTime()
+				.toString()));
 		mPubname.setText(con.getUserInfo().getUserRealName());
 		MypaperAdapter adapter = new MypaperAdapter();
 		mImagePage.setAdapter(adapter);
 		mImagePage.setOnPageChangeListener(pageChangeListener);
 	}
 
-	private void initData()
-	{
+	private void initData() {
 
-		mHandler = new Handler()
-		{
+		mHandler = new Handler() {
 
 			@Override
-			public void handleMessage(Message msg)
-			{
+			public void handleMessage(Message msg) {
 				super.handleMessage(msg);
-				switch (msg.what)
-				{
+				switch (msg.what) {
 				case Constant.HandlerMessageCode.CONTENT_DETAIL_LOAD_DATA_ERROR:
 					headButtonSwitch(DATA_LOAD_FAIL);
-					UIHelper.ToastMessage(LostDetail.this, R.string.msg_load_is_null);
+					UIHelper.ToastMessage(LostDetail.this,
+							R.string.msg_load_is_null);
 					break;
 				case Constant.HandlerMessageCode.CONTENT_DETAIL_LOAD_DATA_SUCCESS:
 					XLostDetail xLostDetail = (XLostDetail) msg.obj;
@@ -170,12 +171,15 @@ public class LostDetail extends BaseActivity
 				case Constant.HandlerMessageCode.COLLECTION_OPERATE_SUCCESS:
 					con.setMeCollecte(!con.isMeCollecte());
 					if (con.isMeCollecte())
-						mHeadRightView.setImageDrawable(resources.getDrawable(R.drawable.head_favorite_y));
+						mHeadRightView.setImageDrawable(resources
+								.getDrawable(R.drawable.head_favorite_y));
 					else
-						mHeadRightView.setImageDrawable(resources.getDrawable(R.drawable.head_favorite));
+						mHeadRightView.setImageDrawable(resources
+								.getDrawable(R.drawable.head_favorite));
 					break;
 				case Constant.HandlerMessageCode.USER_NOT_LOGIN:
-					UIHelper.ToastMessage(LostDetail.this, R.string.user_login_out_of_date);
+					UIHelper.ToastMessage(LostDetail.this,
+							R.string.user_login_out_of_date);
 					UIHelper.showLoginDialog(LostDetail.this);
 					break;
 				default:
@@ -183,29 +187,29 @@ public class LostDetail extends BaseActivity
 				}
 			}
 		};
-		NetControl.getShare(LostDetail.this).getContentDetail(Constant.ContentType.CONTENT_TYPE_LOST,
-				con.getId(), mHandler);
+		NetControl.getShare(LostDetail.this).getContentDetail(
+				Constant.ContentType.CONTENT_TYPE_LOST, con.getId(), mHandler);
 		headButtonSwitch(DATA_LOAD_ING);
 
 	}
 
-	private OnClickListener listener = new OnClickListener()
-	{
+	private OnClickListener listener = new OnClickListener() {
 
 		@Override
-		public void onClick(View v)
-		{
-			switch (v.getId())
-			{
+		public void onClick(View v) {
+			switch (v.getId()) {
 			case R.id.market_detail_footer_btn_call:
-				UIHelper.showCallPhone(LostDetail.this, mPhone.getText().toString());
+				UIHelper.showCallPhone(LostDetail.this, mPhone.getText()
+						.toString());
 				break;
 			case R.id.market_detail_footer_btn_msg:
-				UIHelper.showSendMsg(LostDetail.this, mPhone.getText().toString());
-				break ;
+				UIHelper.showSendMsg(LostDetail.this, mPhone.getText()
+						.toString());
+				break;
 			case R.id.market_detail_footer_btn_words:
-				UIHelper.showLostAndMarketCommentPub(LostDetail.this, con.getId());
-				break ;
+				UIHelper.showLostAndMarketCommentPub(LostDetail.this,
+						con.getId());
+				break;
 			default:
 				break;
 			}
@@ -217,10 +221,8 @@ public class LostDetail extends BaseActivity
 	 * 
 	 * @param type
 	 */
-	private void headButtonSwitch(int type)
-	{
-		switch (type)
-		{
+	private void headButtonSwitch(int type) {
+		switch (type) {
 		case DATA_LOAD_ING:
 			mHeadProgressBar.setVisibility(View.VISIBLE);
 			mHeadRightView.setVisibility(View.GONE);
@@ -236,30 +238,35 @@ public class LostDetail extends BaseActivity
 		}
 	}
 
-	public class MypaperAdapter extends PagerAdapter
-	{
+	public class MypaperAdapter extends PagerAdapter {
 
 		@Override
-		public int getCount()
-		{
+		public int getCount() {
 			return images.size();
 		}
 
 		@Override
-		public boolean isViewFromObject(View arg0, Object arg1)
-		{
+		public boolean isViewFromObject(View arg0, Object arg1) {
 			return arg0 == arg1;
 		}
 
 		@Override
-		public void destroyItem(ViewGroup container, int position, Object object)
-		{
+		public void destroyItem(ViewGroup container, int position, Object object) {
 			container.removeView(images.get(position));
 		}
 
 		@Override
-		public Object instantiateItem(ViewGroup container, int position)
-		{
+		public Object instantiateItem(ViewGroup container, int position) {
+			final int position_ = position;
+			images.get(position).setOnClickListener(new OnClickListener() {
+
+				@Override
+				public void onClick(View v) {
+					// TODO Auto-generated method stub
+					UIHelper.showFullScreenPicture(LostDetail.this, imageList,
+							position_, Constant.PictureType.TYPE_NET);
+				}
+			});
 			container.addView(images.get(position));
 			return images.get(position);
 		}
@@ -269,35 +276,30 @@ public class LostDetail extends BaseActivity
 	/**
 	 * 收藏操作 按钮监听
 	 */
-	private View.OnClickListener collectClickListener = new View.OnClickListener()
-	{
+	private View.OnClickListener collectClickListener = new View.OnClickListener() {
 		@Override
-		public void onClick(View v)
-		{
-			NetControl.getShare(LostDetail.this).collectOperate(con.getId(), con.isMeCollecte(), mHandler);
-//			 NetControl.getShare(LostDetail.this).getContentDetail(Constant.ContentType.CONTENT_TYPE_MARKET,
-//			 con.getId());
-//			 headButtonSwitch(DATA_LOAD_ING);
+		public void onClick(View v) {
+			NetControl.getShare(LostDetail.this).collectOperate(con.getId(),
+					con.isMeCollecte(), mHandler);
+			// NetControl.getShare(LostDetail.this).getContentDetail(Constant.ContentType.CONTENT_TYPE_MARKET,
+			// con.getId());
+			// headButtonSwitch(DATA_LOAD_ING);
 		}
 	};
-	private OnPageChangeListener pageChangeListener = new OnPageChangeListener()
-	{
+	private OnPageChangeListener pageChangeListener = new OnPageChangeListener() {
 
 		@Override
-		public void onPageSelected(int arg0)
-		{
+		public void onPageSelected(int arg0) {
 			indexView.setText((arg0 + 1) + "/" + images.size());
 		}
 
 		@Override
-		public void onPageScrolled(int arg0, float arg1, int arg2)
-		{
+		public void onPageScrolled(int arg0, float arg1, int arg2) {
 
 		}
 
 		@Override
-		public void onPageScrollStateChanged(int arg0)
-		{
+		public void onPageScrollStateChanged(int arg0) {
 		}
 	};
 }
